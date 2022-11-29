afd0c1d0c6862a178f3de95d9a37b9da9f568103
LiamP2000 <liampeeters@live.be>
2022-11-29 19:30:54 +0100

    feat: add DonutCard component + styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
+import DonutCard from './components/DonutCard.vue'
 
 </script>
 
 <template>
+  <div class="app">
+    <h1>Donut Gallery</h1>
+    <div>
+        <DonutCard />
+      </div>
+  </div>
+  
   
 </template>
 
 <style scoped>
+.app {
+  font-family: 'Dosis', sans-serif;
+  text-align: center;
+  color: #E72C70;
+}
 
 </style>
